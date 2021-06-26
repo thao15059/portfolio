3493cc1c66545771ec1b8066a9e54d789d2c9b58
thao15059 <thao15059@gmail.com>
2021-06-26 18:57:16 +0700

    implements services section

--- script.js
+++ script.js
@@ -185,3 +185,21 @@ projectsBtn.addEventListener("click", (e) => {
 
   showHideBool = !showHideBool;
 });
+
+// Section 4
+document.querySelectorAll(".service-btn").forEach((service) => {
+  service.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const serviceText = service.nextElementSibling;
+    serviceText.classList.toggle("change");
+
+    const rightPosition = serviceText.classList.contains("change")
+      ? `calc(100% - ${getComputedStyle(service.firstElementChild).width})`
+      : 0;
+
+    console.log(rightPosition);
+
+    service.firstElementChild.style.right = rightPosition;
+  });
+});
